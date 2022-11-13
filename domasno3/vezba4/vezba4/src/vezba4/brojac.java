e9ec4978c9fc95914a6e67ab87bb712c711635d6
Kristinaa23 <117221036+Kristinaa23@users.noreply.github.com>
2022-11-13 19:37:48 +0100

    Add files via upload

---
+++ domasno3/vezba4/vezba4/src/vezba4/brojac.java
@@ -0,0 +1,22 @@
+package vezba4;
+
+public class brojac {
+
+	public static void main(String[] args) {
+public int brojach;
+		
+		public void zgolemi () {
+			this.brojach++;
+		}
+		
+		public void reset () {
+			this.brojach = 0;
+	
+	}
+
+
+		
+
+	}
+
+
